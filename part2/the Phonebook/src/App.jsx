5fa44d91144684295a18043e2384cf1a45febefa
EloyCato <eloy.catao@gmail.com>
2026-08-14 22:58:59 -0300

    actual 2.8 exercise

--- part2/the Phonebook/src/App.jsx
+++ part2/the Phonebook/src/App.jsx
@@ -1,14 +1,31 @@
 import { useState } from 'react'
 
+const Numbers = ({ persons, findName }) => {
+    console.log(persons)
+    console.log(findName)
+    if(findName === ''){
+      return persons.map( person => <li key={person.id}>{person.name} {person.number}</li>)
+    }
+
+    return persons.filter(person => person.name.toLowerCase().includes(findName)).map( person => <li key={person.name}>{person.name} {person.number}</li>)
+        
+      
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [findName, setFindName] = useState('')
 
   const addContact = (event) => {
     event.preventDefault()
-    const personObject = {name: newName}
+    const personObject = {name: newName, number: newNumber, id: persons.length +1}
 
     if(persons.find(person => person.name === personObject.name) !== undefined){
       //Template literals (Template strings)
@@ -17,28 +34,45 @@ const App = () => {
     }else{
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
-  const handleChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFindChange = (event) =>{
+    setFindName(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: 
+        <input value = {findName} onChange={handleFindChange}/>
+      </div>
+      <h2>Add new</h2>
       <form onSubmit = {addContact}>
         <div>
-          name: <input value={newName} onChange={handleChange}/>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map( person => <li key={person.name}>{person.name}</li>)}
-      </ul> 
+       <ul>
+        <Numbers persons={persons} findName={findName}/>
+       </ul>
     </div>
   )
 }
