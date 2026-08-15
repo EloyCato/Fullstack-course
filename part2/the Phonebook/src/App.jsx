2de1872a66bd662d620f5635a22d30849b2b4183
EloyCato <eloy.catao@gmail.com>
2026-08-15 20:38:42 -0300

    exercise 2.11 completed

--- part2/the Phonebook/src/App.jsx
+++ part2/the Phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const PersonForm = ({addContact, newName, newNumber,handleNameChange,handleNumberChange}) => {
 
@@ -50,15 +51,22 @@ const DisplayNumbers = ({persons, findName}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [findName, setFindName] = useState('')
+
+ const hook = () => {
+  console.log('effect')
+  axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    })
+  }
+  useEffect(hook, [])
+  console.log('render',persons.length, 'people')  
 
   const addContact = (event) => {
     event.preventDefault()
